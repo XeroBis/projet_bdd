b6f07fb0e02fab545d4034f8849d108b76650216
Alan <alanbignon2@gmail.com>
2022-02-25 18:51:03 +0100

    Initial commit

---
+++ create_tables.sql
@@ -0,0 +1,17 @@
+spool create_table.log
+
+prompt *************************************************************
+prompt ******************** DROP TABLE *****************************
+prompt *************************************************************
+
+DROP TABLE COMBATTANT CASCADE CONSTRAINTS;
+DROP TABLE COACH CASCADE CONSTRAINTS;
+DROP TABLE ARBITRE CASCADE CONSTRAINTS;
+DROP TABLE LIEU CASCADE CONSTRAINTS;
+DROP TABLE TOURNOIS CASCADE CONSTRAINTS;
+DROP TABLE PROGRAMME CASCADE CONSTRAINTS;
+
+
+prompt *************************************************************
+prompt ******************** CREATE TABLE ***************************
+prompt *************************************************************
