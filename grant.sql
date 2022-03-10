21118d714f4313a4a8b192accac9b1c896b6b170
Bis <43960612+XeroBis@users.noreply.github.com>
2022-03-10 11:38:25 +0100

    Create grant.sql

---
+++ grant.sql
@@ -0,0 +1,12 @@
+grant select, update, insert, delete on COACH to L3_53;
+grant select, update, insert, delete on COMBATTANT to L3_53;
+grant select, update, insert, delete on LIEU to L3_53;
+grant select, update, insert, delete on ARBITRE to L3_53;
+grant select, update, insert, delete on TOURNOIS to L3_53;
+
+
+grant select, update, insert, delete on COACH to L3_54;
+grant select, update, insert, delete on COMBATTANT to L3_54;
+grant select, update, insert, delete on LIEU to L3_54;
+grant select, update, insert, delete on ARBITRE to L3_54;
+grant select, update, insert, delete on TOURNOIS to L3_54;
