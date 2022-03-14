9d8550860ce487d6c215daf66f5c65cec5e120b9
Bis <43960612+XeroBis@users.noreply.github.com>
2022-03-14 23:03:53 +0100

    added a trigger

--- triggers.sql
+++ triggers.sql
@@ -18,3 +18,22 @@ SHOW ERROR;
 -- to test :
 -- insert INTO TOURNOIS VALUES(1,2,1,2,2,to_date('15-02-2035','dd-mm-yyyy'),3,2);
 -- delete from TOURNOIS WHERE idTournoi =1 AND idCombRouge = 2 and idCombBleu = 1;
+
+CREATE OR REPLACE TRIGGER T_block_ajout_non_gagnant
+BEFORE INSERT ON Tournoi
+FOR EACH ROW
+DECLARE
+  nbGagne NUMBER;
+BEGIN
+  IF EXISTS(SELECT * FROM Tournoi WHERE idTournoi = :new.idTournoi AND Round = :new.Round*2) THEN
+    IF NOT EXISTS (SELECT * FROM Tournoi WHERE idTournoi = :new.idTournoi AND Round = :new.Round*2 AND (idCombRouge = :new.idCombRouge OR idCombBleu = :new.idCombRouge) THEN
+       RAISE_APPLICATION_ERROR( -20002, 'Combattant rouge n\'a pas  gagné dans le round précédent' );
+    ELSE
+       IF NOT EXISTS (SELECT * FROM Tournoi WHERE idTournoi = :new.idTournoi AND Round = :new.Round*2 AND (idCombRouge = :new.idCombBleu OR idCombBleu = :new.idCombBleu) THEN
+          RAISE_APPLICATION_ERROR( -20003, 'Combattant bleu n\'a pas  gagné dans le round précédent' );
+       END IF;
+    END IF;
+ END IF;
+END;
+/
+SHOW ERROR;
