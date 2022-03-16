db74dc168f21fd6189f60d690126d1aa9edb05c2
Bis <43960612+XeroBis@users.noreply.github.com>
2022-03-16 16:20:50 +0100

    Create syn.sql

---
+++ syn.sql
@@ -0,0 +1,5 @@
+create synonym COACH for L3_47.COACH;
+create synonym COMBATTANT for L3_47.COMBATTANT;
+create synonym LIEU for L3_47.LIEU;
+create synonym ARBITRE for L3_47.ARBITRE;
+create synonym TOURNOIS for L3_47.TOURNOIS;
