06ded70028ea50f025e563040c43dacd824ca1ef
Bis <43960612+XeroBis@users.noreply.github.com>
2022-03-16 16:18:42 +0100

    Update grant.sql

--- grant.sql
+++ grant.sql
@@ -1,12 +1,38 @@
+--- L3_51 
+grant select, update, insert, delete on COACH to L3_51;
+grant select, update, insert, delete on COMBATTANT to L3_51;
+grant select, update, insert, delete on LIEU to L3_51;
+grant select, update, insert, delete on ARBITRE to L3_51;
+grant select, update, insert, delete on TOURNOIS to L3_51;
+
+revoke select, update, insert, delete on COACH from L3_51;
+revoke select, update, insert, delete on COMBATTANT from L3_51;
+revoke select, update, insert, delete on LIEU from L3_51;
+revoke select, update, insert, delete on ARBITRE from L3_51;
+revoke select, update, insert, delete on TOURNOIS from L3_51;
+
+--- L3-53
 grant select, update, insert, delete on COACH to L3_53;
 grant select, update, insert, delete on COMBATTANT to L3_53;
 grant select, update, insert, delete on LIEU to L3_53;
 grant select, update, insert, delete on ARBITRE to L3_53;
 grant select, update, insert, delete on TOURNOIS to L3_53;
 
+revoke select, update, insert, delete on COACH from L3_53;
+revoke select, update, insert, delete on COMBATTANT from L3_53;
+revoke select, update, insert, delete on LIEU from L3_53;
+revoke select, update, insert, delete on ARBITRE from L3_53;
+revoke select, update, insert, delete on TOURNOIS from L3_53;
 
+--- L3_54 
 grant select, update, insert, delete on COACH to L3_54;
 grant select, update, insert, delete on COMBATTANT to L3_54;
 grant select, update, insert, delete on LIEU to L3_54;
 grant select, update, insert, delete on ARBITRE to L3_54;
 grant select, update, insert, delete on TOURNOIS to L3_54;
+
+revoke select, update, insert, delete on COACH FROM L3_54;
+revoke select, update, insert, delete on COMBATTANT FROM L3_54;
+revoke select, update, insert, delete on LIEU FROM L3_54;
+revoke select, update, insert, delete on ARBITRE FROM L3_54;
+revoke select, update, insert, delete on TOURNOIS FROM L3_54;
