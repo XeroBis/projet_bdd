9aed7de401c5a27f609cabbe7958bf0af9bc91e7
nathan-art <58117140+nathan-art@users.noreply.github.com>
2022-03-17 10:43:42 +0100

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -1,31 +1,63 @@
--- pour la fonction qui affiche le classement :
-
-SET SERVEROUTPUT ON
-
+SET SERVEROUTPUT ON;
 CREATE OR REPLACE PROCEDURE obtain_classement2
 IS
 BEGIN
-    FOR item IN (SELECT prenom,nom,nbPoint FROM COMBATTANT ORDER BY nbPoint) LOOP
-        dbms_output.put_line('nom = ' || item.nom || ', prenom = ' || item.prenom || ', nombre de point = ' || item.nbPoint );
-    END LOOP;
+	--SET SERVEROUTPUT ON;
+	met_a_jour_score_total;
+
+	FOR item IN (
+		SELECT prenom,nom,nbPoint
+					FROM COMBATTANT
+					ORDER BY nbPoint DESC
+	)
+	LOOP
+
+		dbms_output.put_line('nom = ' || item.nom || ', prenom = ' || item.prenom || ', nombre de point = ' || item.nbPoint );
+
+	END LOOP;
+
 END;
 /
 SHOW ERRORS;
 
-CREATE OR REPLACE PROCEDURE met_a_jour_score(id_Comb NUMBER)
+CREATE OR REPLACE PROCEDURE met_a_jour_score(id IN NUMBER)
+AS
+res number;
+BEGIN
+	--maj du score du joueur
+	UPDATE Combattant
+	SET nbPoint = 0
+	WHERE idComb = id;
+
+	--res := 0;
+	FOR match in ( SELECT Round, idGagnant
+		FROM TOURNOIS
+		WHERE idCombRouge = id OR idCombBleu = id)
+		LOOP
+		IF match.idGagnant = id THEN UPDATE COMBATTANT
+																	SET nbPoint = nbPoint+5--128/match.Round
+																	WHERE idComb = id;
+																	--res := res+128/match.Round;
+		ELSE 										UPDATE COMBATTANT
+																	SET nbPoint = nbPoint-5--128/match.Round
+																	WHERE idComb = id;
+																	--res := res-128/match.Round;
+		END IF;
+		END LOOP;
+END;
+/
+SHOW ERRORS;
+
+
+CREATE OR REPLACE PROCEDURE met_a_jour_score_total
 AS
 BEGIN
-    dbms_output.put_line('id combattant : '|| id_Comb);
-
-    --maj du score du joueur
-    FOR match in ( SELECT Round, idGagnant FROM TOURNOIS WHERE idCombRouge = id_Comb OR idCombBleu = id_Comb)LOOP
-        
-        IF match.idGagnant = id_Comb THEN 
-            UPDATE COMBATTANT SET nbPoint = nbPoint+128/match.Round WHERE idComb = id_Comb;
-        ELSE
-            UPDATE COMBATTANT SET nbPoint = nbPoint-128/match.Round WHERE idComb = id_Comb;
-        END IF;
-    END LOOP;
+	--maj du score du joueur
+	FOR match in ( SELECT idComb
+		FROM COMBATTANT)
+		LOOP
+		met_a_jour_score(match.idComb);
+		END LOOP;
 END;
 /
 SHOW ERRORS;
