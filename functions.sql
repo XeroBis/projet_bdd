4e60182eee1a1f4232a189acb6a2d5d1486477cb
Bis <43960612+XeroBis@users.noreply.github.com>
2022-03-17 11:41:49 +0100

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -1,19 +1,12 @@
 SET SERVEROUTPUT ON;
-CREATE OR REPLACE PROCEDURE obtain_classement2
+CREATE OR REPLACE PROCEDURE get_classement_boxe
 IS
 BEGIN
 	--SET SERVEROUTPUT ON;
 	met_a_jour_score_total;
 
-	FOR item IN (
-		SELECT prenom,nom,nbPoint
-					FROM COMBATTANT
-					ORDER BY nbPoint DESC
-	)
-	LOOP
-
+	FOR item IN (SELECT prenom,nom,nbPoint FROM COMBATTANT ORDER BY nbPoint DESC) LOOP
 		dbms_output.put_line('nom = ' || item.nom || ', prenom = ' || item.prenom || ', nombre de point = ' || item.nbPoint );
-
 	END LOOP;
 
 END;
@@ -29,21 +22,13 @@ BEGIN
 	SET nbPoint = 0
 	WHERE idComb = id;
 
-	--res := 0;
-	FOR match in ( SELECT Round, idGagnant
-		FROM TOURNOIS
-		WHERE idCombRouge = id OR idCombBleu = id)
-		LOOP
-		IF match.idGagnant = id THEN UPDATE COMBATTANT
-																	SET nbPoint = nbPoint+5--128/match.Round
-																	WHERE idComb = id;
-																	--res := res+128/match.Round;
-		ELSE 										UPDATE COMBATTANT
-																	SET nbPoint = nbPoint-5--128/match.Round
-																	WHERE idComb = id;
-																	--res := res-128/match.Round;
+	FOR match in (SELECT Round, idGagnant FROM TOURNOIS	WHERE idCombRouge = id OR idCombBleu = id) LOOP
+		IF match.idGagnant = id THEN 
+			UPDATE COMBATTANT SET nbPoint = nbPoint+5 WHERE idComb = id;
+		ELSE
+			UPDATE COMBATTANT SET nbPoint = nbPoint-5 WHERE idComb = id;
 		END IF;
-		END LOOP;
+	END LOOP;
 END;
 /
 SHOW ERRORS;
@@ -53,11 +38,9 @@ CREATE OR REPLACE PROCEDURE met_a_jour_score_total
 AS
 BEGIN
 	--maj du score du joueur
-	FOR match in ( SELECT idComb
-		FROM COMBATTANT)
-		LOOP
+	FOR match in (SELECT idComb FROM COMBATTANT) LOOP
 		met_a_jour_score(match.idComb);
-		END LOOP;
+	END LOOP;
 END;
 /
 SHOW ERRORS;
